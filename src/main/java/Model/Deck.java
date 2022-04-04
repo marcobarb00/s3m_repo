5d2617f899168ea7ce33e387ff2a2b281f83fa65
Michele Mariani <michele2.mariani@mail.polimi.it>
2022-04-04 11:47:21 +0200

    Creation Deck Class

---
+++ src/main/java/Model/Deck.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.*;
+import java.time.*;
+
+
+public interface Deck {
+
+    void draw();            //Method draws cards to each player
+    void shuffle();         //Method shuffles cards to each player
+
+}
