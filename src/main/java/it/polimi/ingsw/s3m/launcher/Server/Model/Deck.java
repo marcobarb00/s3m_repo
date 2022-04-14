cc901c2d3f406aa69e3cef426bd9abac35895793
Michele Mariani <michele2.mariani@mail.polimi.it>
2022-04-14 19:23:23 +0200

    Deleted classes

--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Deck.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.s3m.launcher.Server.Model;
-
-public interface Deck {
-
-    void draw();            //Method draws cards to each player
-    void shuffle();         //Method shuffles cards to each player
-
-}
