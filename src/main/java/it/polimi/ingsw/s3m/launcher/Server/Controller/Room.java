accd4c956c1cd647d19078a3ec6356dddcd4cf53
Simone-Pedroni-Polimi <10668923@polimi.it>
2022-04-14 13:45:23 +0200

    Fixed Room

--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Controller/Room.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Server/Controller/Room.java
@@ -1,17 +1,10 @@
 package it.polimi.ingsw.s3m.launcher.Server.Controller;
 
-<<<<<<< HEAD
 import it.polimi.ingsw.s3m.launcher.Server.Model.Game;
 
 public class Room {
     private int playersNumber;
     private Game gameState;
 
-    // Provvisoriamente metto il numero di player dal costruttore
-    // per provare a creare il game tramite room
-    public Room (int playersNumber) {
-    }
-=======
-public class Room {
->>>>>>> origin/master
+    public Room() {}
 }
