3059b2780d69bb50d182ce4391a76573e6916063
Michele Mariani <michele2.mariani@mail.polimi.it>
2022-04-15 15:16:00 +0200

    Island modified
    Added next and prev Id as attributes, introduced dominator setter to be called once there's dominance on that island.

--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Island.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Island.java
@@ -1,26 +1,68 @@
 package it.polimi.ingsw.s3m.launcher.Server.Model;
 
 public class Island{
-    private int id;
+    private int id;             //from 0 to 11
+    private int previousIslandId;
+    private int nextIslandId;
     private int towers;
-    private Player dominator;
+    private Player dominator = null;
 
-    public Island(int id, int towers, Player dominator) {
-        this.id = id;
-        this.towers = towers;
-        this.dominator = dominator;
+    static private int idGen = 0;
+
+    public Island() {
+        this.id = idGen;
+        idGen++;
+
+        if(this.id == 11){
+            this.nextIslandId = 0;
+        }else{
+            this.nextIslandId = this.id + 1;
+        }
+
+        if(this.id == 0){
+            this.previousIslandId = 11;
+        }else{
+            this.nextIslandId = this.id - 1;
+        }
     }
 
     public Player computeDominance(){
 
     }
 
+    /**
+     * Adds one tower to the island, it is called inside computeDominance()
+     * @param tower
+     */
     public void setTower(Tower tower){
-
+       this.towers += 1;
     }
 
-    public void mergeIslands(){
-
+    /**
+     * Method to merge islands, how to remove merged island from implementation?
+     * @param islandToMerge
+     */
+    public void mergeIslands(Island islandToMerge){
+        
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public int getTowers() {
+        return towers;
+    }
+
+    /**
+     * Dominator must be set after computeDominance call
+     * @param dominator
+     */
+    public void setDominator(Player dominator) {
+        this.dominator = dominator;
+    }
+
+    public Player getDominator() {
+        return dominator;
+    }
 }
