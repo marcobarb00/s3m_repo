b5cfe11e7ace1f44ca9c44ba1961804ff5f08cb2
Simone-Pedroni-Polimi <10668923@polimi.it>
2022-04-20 16:21:23 +0200

    Operation classes eliminated

--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Game.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Game.java
@@ -3,31 +3,13 @@ package it.polimi.ingsw.s3m.launcher.Server.Model;
 import java.util.ArrayList;
 
 public class Game {
-    /**
-     * Number of players that are playing the game
-     */
     private final int numberOfPlayers;
-    /**
-     * Bag in which are stored the 40 students at the beginning of the game
-     */
     private Bag bag;
-    /**
-     * List of the players in the game
-     */
-    private ArrayList<Player> playersLists;
-    /**
-     * List of the professors in the game
-     */
+    private ArrayList<Player> playersList;
     private ArrayList<Professor> professorsList;
 
-    /**
-     * Constructor of the game in which is given the state of the game.
-     * The game creates the bag,
-     * @param playersList
-     * @return
-     */
     public Game (ArrayList<Player> playersList) {
-        this.playersLists = playersList;
+        this.playersList = playersList;
         numberOfPlayers = playersList.size();
         this.bag = new Bag();
 
@@ -37,22 +19,13 @@ public class Game {
         }
     }
 
-    /**
-     * Method that starts the actual game
-     */
     public void startGame () {
         System.out.println("Game started with " + numberOfPlayers + " players");
     }
 
-    /**
-     * @return professorsList
-     */
     public ArrayList<Professor> getProfessorsList() {
         return professorsList;
     }
 
-    /**
-     * @return bag
-     */
     public Bag getBag () { return bag; }
 }
--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Model/Operation.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.s3m.launcher.Server.Model;
-
-public abstract class Operation {
-    private String nickname;
-
-    public abstract void executeOperation ();
-}
