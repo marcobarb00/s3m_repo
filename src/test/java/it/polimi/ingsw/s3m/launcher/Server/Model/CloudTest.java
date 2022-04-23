2695ce9a134d7672a98376af5e9fbe0e1e940a72
Simone-Pedroni-Polimi <10668923@polimi.it>
2022-04-23 15:30:35 +0200

    Cloud test

--- src/test/java/it/polimi/ingsw/s3m/launcher/Server/Model/CloudTest.java
+++ src/test/java/it/polimi/ingsw/s3m/launcher/Server/Model/CloudTest.java
@@ -7,4 +7,24 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CloudTest {
+
+    @Test
+    void returnThreeStudents() {
+        Cloud cloud = new Cloud(1);
+        ArrayList<Student> setStudents = new ArrayList<>();
+        ArrayList<Student> returnedStudents;
+        returnedStudents = cloud.returnThreeStudents();
+        assertNull(returnedStudents);
+        setStudents.add(new Student(PawnColor.BLUE));
+        setStudents.add(new Student(PawnColor.GREEN));
+        cloud.setStudents(setStudents);
+        assertEquals(2, cloud.getStudents().size());
+        returnedStudents = cloud.returnThreeStudents();
+        assertNull(returnedStudents);
+        setStudents.add(new Student(PawnColor.PINK));
+        assertEquals(3, cloud.getStudents().size());
+        returnedStudents = cloud.returnThreeStudents();
+        assertEquals(3, returnedStudents.size());
+        assertEquals(0, cloud.getStudents().size());
+    }
 }
