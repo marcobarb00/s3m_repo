cc3763a4e1b50f42f18f50265914b49285b0f455
Michele Mariani <michele2.mariani@mail.polimi.it>
2022-04-30 15:43:18 +0200

    Update ActivateJesterEffectOperation

--- src/main/java/it/polimi/ingsw/s3m/launcher/Server/Controller/ActivateJesterEffectOperation.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Server/Controller/ActivateJesterEffectOperation.java
@@ -2,14 +2,12 @@ package it.polimi.ingsw.s3m.launcher.Server.Controller;
 
 import it.polimi.ingsw.s3m.launcher.Server.Exception.CloudNotInListException;
 import it.polimi.ingsw.s3m.launcher.Server.Exception.PlayerNotInListException;
-import it.polimi.ingsw.s3m.launcher.Server.Model.Cloud;
-import it.polimi.ingsw.s3m.launcher.Server.Model.Game;
-import it.polimi.ingsw.s3m.launcher.Server.Model.Student;
+import it.polimi.ingsw.s3m.launcher.Server.Model.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ActivateJesterEffectOperation extends Operation{
-    private PlayerController playerController;
     private ArrayList<Student> requiredStudents;
     private ArrayList<Student> givenStudents;
 
@@ -25,29 +23,67 @@ public class ActivateJesterEffectOperation extends Operation{
     public ActivateJesterEffectOperation(Game game, PlayerController playerController,
                                          ArrayList<Student> requiredStudents,
                                          ArrayList<Student> givenStudents) {
-        super(game);
-        this.playerController = playerController;
+        super(game, playerController);
         this.requiredStudents = requiredStudents;
         this.givenStudents = givenStudents;
     }
 
+    //TODO
     @Override
     public void executeOperation() throws PlayerNotInListException, IllegalArgumentException{
-        ArrayList<String> playersList = super.game.getPlayersNicknames();
-
-        boolean playerControllerInList = playersList.contains(playerController.getNickname());
+        //Check for double nicknames
+        boolean playerControllerInList = checkNickname();
         if(!playerControllerInList){
             throw new PlayerNotInListException();
         }
 
         boolean checkRequired = requiredStudents.size() == 3;
         boolean checkGiven = givenStudents.size() == 3;
+        if(!(checkGiven && checkRequired)){
+            //throw new IllegalArgumentException("Must exchange only 3 students");
+        }
 
-        if(checkGiven && checkRequired){
-            super.game.activateJesterEffect(playerController.getNickname(),
-                    requiredStudents, givenStudents);
-        }else{
-            throw new IllegalArgumentException();
+        //Search students on card
+        searchStudentsOnCard();
+
+        //Search students in dashboard hall
+        //Smells soooo bad
+        searchStudentsInHall();
+
+        super.game.activateJesterEffect(playerController.getNickname(),
+                requiredStudents, givenStudents);
+    }
+
+    private void searchStudentsOnCard(){
+        HashMap<PawnColor, Integer> studentsOnJester = game.getJesterStudentsOnCard();
+        for(Student requiredStudent : requiredStudents){
+            PawnColor requiredStudentColor = requiredStudent.getColor();
+            boolean checkOnCard = studentsOnJester.get(requiredStudentColor) > 0;
+            if(checkOnCard){
+                int oldValue = studentsOnJester.get(requiredStudent.getColor());
+                studentsOnJester.replace(requiredStudentColor, oldValue - 1);
+            }else{
+                throw new IllegalArgumentException("Student not on Jester");
+            }
+        }
+    }
+
+    private void searchStudentsInHall(){
+        String playerNickname = playerController.getNickname();
+        Player player = game.getPlayerHashMap().get(playerNickname);
+        ArrayList<Student> hall = player.getDashboard().getHall();
+
+        for(Student givenStudent: givenStudents){
+            boolean checkStudentInHall = false;
+            for(Student studentInHall : hall){
+                checkStudentInHall = studentInHall.getColor() == givenStudent.getColor();
+                if(checkStudentInHall){
+                    hall.remove(studentInHall);
+                    break;
+                }
+            }
+            if(!checkStudentInHall)
+                throw new IllegalArgumentException("Student not in hall");
         }
     }
 }
