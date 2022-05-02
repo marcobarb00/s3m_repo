c1d55596f69dc7d59f4054e0a84e97e963905c86
marco-barbieri-polimi <10684602@polimi.it>
2022-05-02 19:27:38 +0200

    fixed view interface

--- src/main/java/it/polimi/ingsw/s3m/launcher/Client/View/GUI/GUIView.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Client/View/GUI/GUIView.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.s3m.launcher.Client.View.GUI;
 
+import it.polimi.ingsw.s3m.launcher.Client.View.View;
 import it.polimi.ingsw.s3m.launcher.Communication.*;
 
 public class GUIView extends View{
