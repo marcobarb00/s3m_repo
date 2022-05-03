90c8adaffeaa951dfc8a437533a1767aaf93302d
Matteo Bianchi <85030042+blancosss21@users.noreply.github.com>
2022-05-03 17:53:37 +0200

    start gui
    start gui

---
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Client/View/GuiController/StartGUI.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.s3m.launcher.Client.View.GuiController;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
