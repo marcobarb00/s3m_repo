102bd16b8aadf5df933cec64ee093fc2dad1eea0
Matteo Bianchi <85030042+blancosss21@users.noreply.github.com>
2022-05-13 16:13:50 +0200

    hhh

--- src/main/java/it/polimi/ingsw/s3m/launcher/Client/View/GUI/EnterRoomGUI.java
+++ src/main/java/it/polimi/ingsw/s3m/launcher/Client/View/GUI/EnterRoomGUI.java
@@ -6,7 +6,6 @@ import it.polimi.ingsw.s3m.launcher.Server.Message.EnterRoomMessage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
